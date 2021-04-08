c38ba14645138b557e122785eb1e4e7e9abfd32c
milanbargiel <milan.bargiel@googlemail.com>
2021-04-08 17:37:28 +0200

    [CMS] Find orders based on keys

--- cms/api/order/controllers/order.js
+++ cms/api/order/controllers/order.js
@@ -5,4 +5,19 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { key } = ctx.params;
+
+    const entity = await strapi.services.order.findOne({ key });
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+};
