1285c5e2490d5b4c8dda362163ba1e6d41e7a8c1
milanbargiel <milan.bargiel@googlemail.com>
2021-04-13 19:56:24 +0200

    [App] Add missing sand css

--- app/components/SandSimulation.vue
+++ app/components/SandSimulation.vue
@@ -29,8 +29,18 @@ export default {
 
 <style>
 canvas {
-  width: 100vw;
-  height: 100vh;
+  pointer-events: none;
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  width: 100vw !important;
+  height: 100vh !important;
+  min-width: 600px;
+  min-height: 600px;
   image-rendering: pixelated;
+  image-rendering: -moz-crisp-edges;
+  image-rendering: -webkit-crisp-edges;
+  image-rendering: crisp-edges;
 }
 </style>
