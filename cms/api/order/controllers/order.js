c1f79da736112b0a115e3709f133c2808f1cc74f
milanbargiel <milan.bargiel@googlemail.com>
2021-04-18 18:19:56 +0200

    [CMS] Set up order controller for Stripe checkout

--- cms/api/order/controllers/order.js
+++ cms/api/order/controllers/order.js
@@ -5,9 +5,10 @@
  * to customize this controller
  */
 
-const { sanitizeEntity } = require('strapi-utils');
+ const { sanitizeEntity } = require('strapi-utils');
+ const stripe = require('stripe')('sk_test_51Ig9QCHHn9ZSZEneIp03I7pkwIUj89ErFNcQ3e7GZSVc5zZzHvDRImdLz3seEJ3zCr5Rbhqoz9KwMjHG6HoyG7U200qUFctzo9');
 
-module.exports = {
+ module.exports = {
   // Retrieve an order by its key (secret url slug) instead of numerical id
   async findOne(ctx) {
     const { key } = ctx.params;
@@ -26,5 +27,43 @@ module.exports = {
     });
 
     return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+
+  // Create order
+  async create(ctx) {
+
+    // Charge the customer
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+        {
+          price_data: {
+            currency: 'eur',
+            product_data: {
+              name: ctx.request.body.description,
+            },
+            unit_amount: ctx.request.body.price, // price is in cents
+          },
+          quantity: 1,
+        },
+        ],
+        mode: 'payment',
+        success_url: 'https://example.com/success',
+        cancel_url: 'https://example.com/cancel',
+      });
+
+      // Save the order in the database
+      try {
+        const entity = await strapi.services.order.create(ctx.request.body);
+
+        // Return Stripe Checkout Session ID
+        return { id: session.id };
+      } catch (err) {
+        console.log(err)
+      }
+    } catch (err) {
+      console.log(err)
+    }
   },
 };
--- cms/config/server.js
+++ cms/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://xyz.timesales.ltd/',
+  url: 'http://localhost:1337',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '61862e3e067c515d62fbb8698852447e'),
