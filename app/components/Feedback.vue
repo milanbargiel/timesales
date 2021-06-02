16dc6f98d8476fad07be6b27a535f13d9f313ec2
milanbargiel <milan.bargiel@googlemail.com>
2021-06-02 19:54:52 +0200

    Offer more time to buy in feedback conversation

--- app/components/Feedback.vue
+++ app/components/Feedback.vue
@@ -21,6 +21,17 @@ export default {
     this.startConversation()
   },
   methods: {
+    async moreTime() {
+      await this.botMessage('Do you want to have some more <i>time</i>?')
+      const response = await this.botYesOrNo()
+
+      if (response.value === true) {
+        // Go to checkout flow on index page
+        this.$router.push('/')
+      } else {
+        await this.botMessage('Great. I hope to welcome you soon again.)')
+      }
+    },
     async startConversation() {
       await this.botMessage('Did you enjoy your time?')
 
@@ -28,18 +39,30 @@ export default {
         'Leave your thoughts, remarks, critique. (We reserve the rights to use them anonymised)'
       )
 
+      const feedback = await (() => {
+        // Pushy question when user does not starts typing within 10 seconds
+        const t = setTimeout(async () => {
+          const textInput = this.$el.querySelector('.botui-actions-text-input')
+
+          // If no input was given, hide input field and continue dialogue
+          if (!textInput.value) {
+            textInput.hidden = true
+            await this.moreTime()
+          }
+        }, 1000)
+
+        // Ask for feedback
+        return this.botTextInput('Your feedback').then((response) => {
+          // Do not show pushy questions anymore when feedback is given
+          clearTimeout(t)
+          return response
+        })
+      })()
+
+      // Ask, if the user wants to buy more time
       // Save feedback TODO: Request to actually save the feedback in the database
-      this.botTextInput('Your feedback')
-
-      await this.timeout(7000)
-
-      await this.botMessage('Do you want to have some more <i>time</i>?)')
-
-      const response = await this.botYesOrNo()
-
-      if (response.value === true) {
-        // Go to checkout flow on index page
-        this.$router.push('/')
+      if (feedback) {
+        await this.moreTime()
       }
     },
   },
