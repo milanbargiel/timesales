63e56de341b4dbc8df6d892fb77f3238a07aff11
milanbargiel <milan.bargiel@googlemail.com>
2021-07-17 17:57:39 +0200

    Change layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -27,6 +27,14 @@
         </p>
       </div>
     </div>
+    <div class="purchase-ticker">
+      <div class="purchase">
+        Dan has just bought time for <span class="euro">20 €</span>
+      </div>
+      <div class="purchase">
+        Sarah has just bought time for <span class="euro">10 €</span>
+      </div>
+    </div>
     <footer class="footer">
       <div class="navigation">
         <a href="#">Imprint</a>
