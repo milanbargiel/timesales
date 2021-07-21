32c7957f09205b2390f32eaaeaa01d00b719dcd1
milanbargiel <milan.bargiel@googlemail.com>
2021-07-21 12:38:03 +0200

    Add Review component

---
+++ app/components/Review.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="review">
+    <button class="close-btn close-btn--review">x</button>
+    {{ reviewText }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    reviewText: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
--- app/components/Reviews.vue
+++ app/components/Reviews.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="review-container">
-    <div class="review">
-      <button class="close-btn close-btn--review">x</button>
-      “That was a great service, I would really recommend it” – Peter
-    </div>
-    <div class="review">
-      <button class="close-btn close-btn--review">x</button>
-      “It was horrible” – Anna
-    </div>
+    <Review
+      review-text="“That was a great service, I would really recommend it” – Peter"
+    />
+    <Review review-text="“It was horrible” – Anna" />
   </div>
 </template>
