07c1b164e4d4e01c52cd1cfdef441cd7f38a71a5
milanbargiel <milan.bargiel@googlemail.com>
2021-07-21 14:46:59 +0200

    Randomly generate position of reviews on screen

--- app/components/Review.vue
+++ app/components/Review.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="review">
+  <div class="review" :style="positionStyles">
     <button class="close-btn close-btn--review">x</button>
     “{{ reviewText }}” <span class="review-author">– {{ reviewAuthor }}</span>
   </div>
@@ -15,6 +15,38 @@ export default {
     reviewAuthor: {
       type: String,
       required: true,
+    },
+  },
+  data() {
+    return { windowWidth: null }
+  },
+  computed: {
+    isDesktop() {
+      return this.windowWidth >= 680 // breakpoint as defined in stylesheets
+    },
+    positionStyles() {
+      const position = {
+        top: this.getRandomInt(0, 70) + '%',
+        right: this.getRandomInt(0, 80) + '%',
+      }
+
+      return this.isDesktop ? position : ''
+    },
+  },
+  mounted() {
+    // Set windowWidth when component is mounted
+    this.windowWidth = window.innerWidth
+
+    // Listen for changes
+    window.addEventListener('resize', () => {
+      this.windowWidth = window.innerWidth
+    })
+  },
+  methods: {
+    getRandomInt(min, max) {
+      min = Math.ceil(min)
+      max = Math.floor(max)
+      return Math.floor(Math.random() * (max - min + 1)) + min
     },
   },
 }
