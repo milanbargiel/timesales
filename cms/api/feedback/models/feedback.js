2e8e9a5b228a89444c58d75c3878c58f2ba3b135
milanbargiel <milan.bargiel@googlemail.com>
2021-07-22 10:04:03 +0200

    Create fakeAuthor on feedback creation

--- cms/api/feedback/models/feedback.js
+++ cms/api/feedback/models/feedback.js
@@ -1,8 +1,20 @@
 'use strict';
+const Chance = require('chance-de')
+const chance = new Chance()
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+ lifecycles: {
+    async beforeCreate(data) {
+      // Generate fake name for feedback
+      data.fakeAuthor = chance.first({nationality: 'de'});
+      
+      // Do not publish feedback immediately
+      data.published_at = null;
+    },
+  },
+};
