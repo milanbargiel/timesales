fc0bb0da32b6d671b7a112bc39f3e6495729b8a7
milanbargiel <milan.bargiel@googlemail.com>
2021-07-28 10:43:13 +0200

    Open footer on click

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,40 +1,56 @@
 <template>
-  <footer v-if="showFooter" class="footer">
-    <div class="creators">
-      <div class="creators-header">Partners:</div>
-      <a class="link link--underlined" href="#">Milan Bargiel</a>
-      <a class="link link--underlined" href="#">Jim-Fx</a>
-      <a class="link link--underlined" href="#">Katherina Gorodynska</a>
-      <a class="link link--underlined" href="#">Ludwig Lederer</a>
-      <a class="link link--underlined" href="#">Biomigrant</a>
-    </div>
-    <div class="navigation">
-      <a class="link" href="#">Imprint</a>
-      <a class="link" href="#">Data privacy</a>
-      <a class="link" href="#">Terms and conditions</a>
-    </div>
-    <div class="patrons">
-      <img
-        class="patron-logo"
-        src="~/assets/lab-k-nrw.svg"
-        alt="Landesbüro für Bildende Kunst (LaB K)"
-      />
-      <div class="debug-toggle">
-        <label class="switch">
-          <input v-model="debugMode" type="checkbox" @click="toggleDebugMode" />
-          <div></div>
-        </label>
-        Fast conversation
+  <div v-if="showFooter" class="footer-container">
+    <footer v-if="footerUnfolded" class="footer">
+      <div></div>
+      <div class="creators">
+        <div class="creators-header">Partners:</div>
+        <a class="link link--underlined" href="#">Milan Bargiel</a>
+        <a class="link link--underlined" href="#">Jim-Fx</a>
+        <a class="link link--underlined" href="#">Katherina Gorodynska</a>
+        <a class="link link--underlined" href="#">Ludwig Lederer</a>
+        <a class="link link--underlined" href="#">Biomigrant</a>
       </div>
-    </div>
-    <Triangle class="footer-toggle" />
-  </footer>
+      <div class="navigation">
+        <a class="link" href="#">Imprint</a>
+        <a class="link" href="#">Data privacy</a>
+        <a class="link" href="#">Terms and conditions</a>
+      </div>
+      <div class="patrons">
+        <img
+          class="patron-logo"
+          src="~/assets/lab-k-nrw.svg"
+          alt="Landesbüro für Bildende Kunst (LaB K)"
+        />
+        <div class="debug-toggle">
+          <label class="switch">
+            <input
+              v-model="debugMode"
+              type="checkbox"
+              @click="toggleDebugMode"
+            />
+            <div></div>
+          </label>
+          Fast conversation
+        </div>
+      </div>
+    </footer>
+    <Triangle
+      class="footer-toggle"
+      :unfolded="footerUnfolded"
+      @click.native="toggleFooter"
+    />
+  </div>
 </template>
 
 <script>
 import { mapMutations } from 'vuex' // helper for mapping vuex store mutations to methods
 
 export default {
+  data() {
+    return {
+      footerUnfolded: false,
+    }
+  },
   computed: {
     debugMode() {
       return this.$store.state.ui.debugMode
@@ -49,6 +65,9 @@ export default {
       // Enables this.toggleDebugMode()
       toggleDebugMode: 'ui/toggleDebugMode',
     }),
+    toggleFooter() {
+      this.footerUnfolded = !this.footerUnfolded
+    },
   },
 }
 </script>
--- app/components/Triangle.vue
+++ app/components/Triangle.vue
@@ -5,7 +5,24 @@
     viewBox="0 0 38 26"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    :transform="rotation"
   >
     <path d="M19 0L37.1865 25.5H0.813467L19 0Z" fill="black" />
   </svg>
 </template>
+
+<script>
+export default {
+  props: {
+    unfolded: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    rotation() {
+      return this.unfolded ? 'rotate(180)' : ''
+    },
+  },
+}
+</script>
