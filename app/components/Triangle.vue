8d896a70679c3fb19fd11073cfcc5ad1f7b8c291
milanbargiel <milan.bargiel@googlemail.com>
2021-07-29 19:54:19 +0200

    Fix trinagle rotation safari bug

--- app/components/Triangle.vue
+++ app/components/Triangle.vue
@@ -1,14 +1,26 @@
 <template>
-  <svg
-    width="38"
-    height="26"
-    viewBox="0 0 38 26"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    :transform="rotation"
-  >
-    <path d="M19 0L37.1865 25.5H0.813467L19 0Z" fill="black" />
-  </svg>
+  <div>
+    <svg
+      v-if="unfolded"
+      width="38"
+      height="26"
+      viewBox="0 0 38 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M19 26L0.813469 0.5L37.1865 0.5L19 26Z" fill="black" />
+    </svg>
+    <svg
+      v-else
+      width="38"
+      height="26"
+      viewBox="0 0 38 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M19 0L37.1865 25.5H0.813467L19 0Z" fill="black" />
+    </svg>
+  </div>
 </template>
 
 <script>
@@ -17,11 +29,6 @@ export default {
     unfolded: {
       type: Boolean,
       required: true,
-    },
-  },
-  computed: {
-    rotation() {
-      return this.unfolded ? 'rotate(180)' : ''
     },
   },
 }
