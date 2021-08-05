d335c0692be812691f43b21430cc5874c9d0f9a2
milanbargiel <milan.bargiel@googlemail.com>
2021-08-05 16:13:28 +0200

    Use ascii close symbol for header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="fade">
     <header v-if="showHeader" class="header">
-      <button class="close-btn close-btn--header" @click="hideHeader">x</button>
+      <button class="close-btn close-btn--header" @click="hideHeader">×</button>
       <h1 class="header-title">Time Sales Online</h1>
     </header>
   </transition>
