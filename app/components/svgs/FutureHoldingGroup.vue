875b290f55eb91868c03d28ff147df4c8b26e65c
milanbargiel <milan.bargiel@googlemail.com>
2022-01-31 13:51:29 +0100

    Minify founder svgs

--- app/components/svgs/FutureHoldingGroup.vue
+++ app/components/svgs/FutureHoldingGroup.vue
@@ -2,76 +2,33 @@
   <a href="https://www.futureholdinggroup.de/" target="_blank">
     <svg
       class="patron-logo patron-logo--future-holding"
-      id="Ebene_1"
-      data-name="Ebene 1"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 273.85 206.08"
+      xmlns:v="https://vecta.io/nano"
     >
-      <defs>
-        <style>
-          .patron-logo--future-holding .cls-1 {
-            fill: #fff;
-          }
-          .patron-logo--future-holding .cls-2,
-          .patron-logo--future-holding .cls-3 {
-            fill: none;
-            stroke: #1d1d1b;
-            stroke-miterlimit: 10;
-            stroke-width: 3px;
-          }
-          .patron-logo--future-holding .cls-3 {
-            stroke-dasharray: 109.52 44.8;
-          }
-          .patron-logo--future-holding .cls-4 {
-            font-size: 31.38px;
-            fill: #1d1d1b;
-            font-family: FunktionalGrotesk-Book, Funktional Grotesk, 'Arial',
-              'Archivo', sans-serif;
-          }
-          .patron-logo--future-holding .cls-5 {
-            letter-spacing: -0.01em;
-          }
-          .patron-logo--future-holding .cls-6 {
-            letter-spacing: 0;
-          }
-          .patron-logo--future-holding .cls-7 {
-            letter-spacing: 0;
-          }
-          .patron-logo--future-holding .cls-8 {
-            letter-spacing: 0;
-          }
-          .patron-logo--future-holding .cls-9 {
-            letter-spacing: -0.01em;
-          }
-          .patron-logo--future-holding .cls-10 {
-            letter-spacing: 0.01em;
-          }
-        </style>
-      </defs>
-      <polygon
-        class="cls-1"
-        points="4.03 2.61 4.03 158.22 271.13 2.61 271.13 158.22 4.03 2.61"
-      />
-      <polyline class="cls-2" points="51.55 30.3 4.03 2.61 4.03 57.61" />
-      <polyline class="cls-2" points="4.03 103.22 4.03 158.22 51.55 130.53" />
-      <line class="cls-3" x1="90.26" y1="107.98" x2="204.25" y2="41.57" />
-      <polyline class="cls-2" points="223.61 30.3 271.13 2.61 271.13 57.61" />
-      <polyline
-        class="cls-2"
-        points="271.13 103.22 271.13 158.22 223.61 130.53"
-      />
-      <line class="cls-3" x1="184.9" y1="107.98" x2="70.91" y2="41.57" />
-      <text class="cls-4" transform="translate(0 199.33) scale(0.94 1)">
-        <tspan class="cls-5">F</tspan>
-        <tspan class="cls-6" x="17.26" y="0">ut</tspan>
-        <tspan class="cls-7" x="43.78" y="0">u</tspan>
-        <tspan class="cls-5" x="60.32" y="0">r</tspan>
+      <path d="M4.03 2.61v155.61L271.13 2.61v155.61L4.03 2.61z" fill="#fff" />
+      <g fill="none" stroke="#1d1d1b" stroke-miterlimit="10" stroke-width="3">
+        <path d="M51.55 30.3L4.03 2.61v55m0 45.61v55l47.52-27.69" />
+        <path d="M90.26 107.98l113.99-66.41" stroke-dasharray="109.52 44.8" />
+        <path d="M223.61 30.3l47.52-27.69v55m0 45.61v55l-47.52-27.69" />
+        <path d="M184.9 107.98L70.91 41.57" stroke-dasharray="109.52 44.8" />
+      </g>
+      <text
+        transform="matrix(.94 0 0 1 0 199.33)"
+        font-size="31.38"
+        fill="#1d1d1b"
+        font-family="FunktionalGrotesk-Book,Funktional Grotesk"
+      >
+        <tspan class="E">F</tspan>
+        <tspan class="F" x="17.26" y="0">ut</tspan>
+        <tspan class="G" x="43.78" y="0">u</tspan>
+        <tspan class="E" x="60.32" y="0">r</tspan>
         <tspan x="70.8" y="0">e</tspan>
-        <tspan class="cls-8" x="94.84" y="0">H</tspan>
-        <tspan class="cls-9" x="116.93" y="0">ol</tspan>
-        <tspan class="cls-10" x="140.66" y="0">d</tspan>
+        <tspan class="H" x="94.84" y="0">H</tspan>
+        <tspan class="I" x="116.93" y="0">ol</tspan>
+        <tspan class="J" x="140.66" y="0">d</tspan>
         <tspan x="158.39" y="0">ing G</tspan>
-        <tspan class="cls-5" x="228.72" y="0">r</tspan>
+        <tspan class="E" x="228.72" y="0">r</tspan>
         <tspan x="239.2" y="0">oup</tspan>
       </text>
     </svg>
