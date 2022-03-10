6a90d87e2b4627e8cb01ae6f940707eea2f7c454
milanbargiel <milan.bargiel@googlemail.com>
2022-03-10 11:37:03 +0100

    Show numberOfWords in vuex store

--- app/store/config.js
+++ app/store/config.js
@@ -1,6 +1,8 @@
 const state = () => ({
   aiConfig: {
+    // Set default values that are overwritten by fetchConfigData
     milliSecondsToWait: 5000,
+    numberOfWords: 10,
   },
 })
 
@@ -8,12 +10,16 @@ const mutations = {
   setGpt2Timeout(state, value) {
     state.aiConfig.milliSecondsToWait = value
   },
+  setNumberOfWords(state, value) {
+    state.aiConfig.numberOfWords = value
+  },
 }
 
 const actions = {
   fetchConfigData({ commit }) {
     this.$axios.$get(`${this.$config.apiUrl}/config`).then((data) => {
       commit('setGpt2Timeout', data.aiConfig.milliSecondsToWait)
+      commit('setNumberOfWords', data.aiConfig.numberOfWords)
     })
   },
 }
