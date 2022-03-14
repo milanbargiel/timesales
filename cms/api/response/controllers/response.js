c276ebf4d9a76d4bb4d8cda8593c11bab7088a78
milanbargiel <milan.bargiel@googlemail.com>
2022-03-14 11:46:09 +0100

    Refactor generateAiComment controller action

--- cms/api/response/controllers/response.js
+++ cms/api/response/controllers/response.js
@@ -7,6 +7,35 @@
 
 const axios = require('axios');
 
+const processAiOutput = (aiOutput) => {
+  let processedOutput;
+
+  // Check first 20 characters
+  // If there is a point or a comma, remove everything untill there
+  const firstPunctuationMark = aiOutput.search(/[.,]/g);
+  // Remove everything from the end untill the last punctuation mark
+  const lastPunctuationMark = aiOutput.lastIndexOf('.');
+
+  // Only apply changes when the punctuation marks are not the same
+  if (lastPunctuationMark > firstPunctuationMark) {
+    processedOutput = aiOutput
+      .substring(
+        firstPunctuationMark + 1, // remove punctuation mark
+        lastPunctuationMark + 1 // keep last punctuation mark
+      )
+      .trim(); // remove whitespace from beginning
+
+    // Capitalize first letter
+    processedOutput =
+      processedOutput.charAt(0).toUpperCase() + processedOutput.slice(1);
+  } else {
+    // Else use regular aiOutput
+    processedOutput = aiOutput;
+  }
+
+  return processedOutput;
+};
+
 module.exports = {
   /**
    * Update a record.
@@ -15,7 +44,7 @@ module.exports = {
    */
 
   async generateAiComment(ctx) {
-    const { id } = ctx.params;
+    const { id } = ctx.params; // Id of response
 
     // Get ai configuration
     const config = await strapi.services.config.find();
@@ -25,9 +54,9 @@ module.exports = {
       return ctx.badRequest('aiConfig is not set');
     }
 
+    // 1. Iterate over fields from the request data and get the userInput for ai comment generation
     const requestBody = ctx.request.body;
 
-    // Iterate over fields from the request data and get the userInput for ai comment generation
     let fieldName, userInput, aiOutput, enhancedOutput;
 
     for (var key in requestBody) {
@@ -41,13 +70,13 @@ module.exports = {
       }
     }
 
-    // Send userInput to the GPT2 app to generate an ai comment
+    // 2. Send userInput to the GPT2 app to generate an ai comment
     await axios
       .post(
         strapi.config.get('server.gpt2Api'),
         {
           prefix: userInput,
-          length: config.aiConfig.numberOfWords, // number of words that ai responds withs
+          length: config.aiConfig.numberOfWords, // number of words that ai shall respond withs
           temperature: config.aiConfig.temperature,
           top_k: config.aiConfig.topK
         },
@@ -55,70 +84,54 @@ module.exports = {
       )
       .then((response) => {
         aiOutput = response.data.text;
-
-        // enhance output to make it meaningfull
-        // check first 20 characters
-        // if there is a point or a comma, remove everything untill there
-        const firstPunctuationMark = aiOutput.search(/[.,]/g);
-        // remove everything from the end untill the last punctuation mark
-        const lastPunctuationMark = aiOutput.lastIndexOf('.');
-
-        // Only apply changes when the punctuation marks are not the same
-        if (lastPunctuationMark > firstPunctuationMark) {
-          enhancedOutput = aiOutput
-            .substring(
-              firstPunctuationMark + 1, // remove punctuation mark
-              lastPunctuationMark + 1 // keep last punctuation mark
-            )
-            .trim(); // remove whitespace from beginning
-
-          // Capitalize first letter
-          enhancedOutput =
-            enhancedOutput.charAt(0).toUpperCase() + enhancedOutput.slice(1);
-        } else {
-          // Else use regular aiOutput
-          enhancedOutput = aiOutput;
-        }
       })
-      .catch((error) => {
+      .catch(async () => {
         // When the requests runs into a timeout or when the gpt2 app is down
-        console.log(error);
-        aiOutput = undefined;
-        enhancedOutput = undefined;
+        // Only save the userInput for the specific field
+        try {
+          const userInput = {
+            [fieldName]: {
+              userInput
+            }
+          };
+          await strapi.services.response.update({ id }, userInput);
+          return userInput;
+        } catch (error) {
+          // Throw an error when userInput could not be saved
+          return ctx.badImplementation(error);
+        }
       });
 
-    const aiComment = {
-      [fieldName]: {
-        userInput,
-        aiOutput,
-        enhancedOutput
-      }
+    // 3. Process aiOutput and make it meaningful
+    enhancedOutput = processAiOutput(aiOutput);
+
+    const aiGeneratedData = {
+      userInput,
+      aiOutput,
+      enhancedOutput
     };
 
-    // Push aiComment to array for repeatable comments
-    if (fieldName === 'isItGodComments') {
-      const response = await strapi.services.response.findOne({ id });
-      // Save userInput and aiOutput in the database
+    // 4. Save data in database
+    const response = await strapi.services.response.findOne({ id });
+
+    // 4A. If field type is a repeatable component (e.g. isItGodComments), add to array
+    if (Array.isArray(response[fieldName])) {
       await strapi.services.response.update(
         { id },
         {
-          [fieldName]: [
-            ...response[fieldName],
-            {
-              userInput,
-              aiOutput,
-              enhancedOutput
-            }
-          ]
+          [fieldName]: [...response[fieldName], aiGeneratedData]
         }
       );
 
-      return aiComment;
+      return { [fieldName]: aiGeneratedData };
     }
+    // 4B. If field type is a single component update response
+    await strapi.services.response.update(
+      { id },
+      { [fieldName]: aiGeneratedData }
+    );
 
-    // Save userInput and aiOutput in the database
-    await strapi.services.response.update({ id }, aiComment);
-
-    return aiComment;
+    // Return data to display it in the bot conversation of the frontend
+    return { [fieldName]: aiGeneratedData };
   }
 };
